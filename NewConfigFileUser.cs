62a51a877ba70f45b19ba91b328e21b80beba962
Progrmm <michal.pelon@proton.me>
2024-04-03 19:57:33 +0200

    Update NewConfigFileUser.cs

--- NewConfigFileUser.cs
+++ NewConfigFileUser.cs
@@ -12,7 +12,8 @@
 
         public string filePath;
         public string[] fileLines;
-        public List<string> categories = new List<string>();
+
+        public List<string> categories = [];
         public Dictionary<string, Dictionary<string, object>> values;
 
         public void RepairFile()
@@ -22,13 +23,32 @@
 
         public void Map()
         {
-            bool valuesLine = false;
+            bool valuesLine = false, objectVal = false;
             string category = "";
+            int lnum = -1;
+
             foreach (var line in fileLines)
             {
+                lnum++;
                 string l = line.Trim();
                 if (l == "")
                     continue;
+
+                if(l.StartsWith('$')||objectVal)
+                {
+                    if (l == "{")
+                        continue;
+
+                    if(!objectVal)
+                        ReadObject(lnum, category, l.Replace("$",""));
+
+                    objectVal = true;
+
+                    if(l == "}")
+                        objectVal = false;
+
+                    continue;
+                }
 
                 if (l == "{")
                 {
@@ -47,40 +67,88 @@
                 }
 
 
-                values.Add(l, new Dictionary<string, object>());
+                values.Add(l, []);
                 category = l;
             }
 
         }
 
-        public string GetVarName(string line)
+        public void ReadObject(int lnum, string category, string objname)
         {
-            return line.Remove(line.IndexOf(":")).Trim();
+            lnum+=2;
+
+            values[category].Add(objname, "");
         }
+
+        public string GetVarName(string line) => line.Remove(line.IndexOf(":")).Trim();
 
         public object GetVarValue(string line)
         {
-            line = line.Substring(line.IndexOf(":")).Trim();
+            line = line[line.IndexOf(":")..].Trim();
 
+            switch(GetType(line))
+            {
+                case "String":
+                    return line;
+
+                case "Int":
+                    return Convert.ToInt32(line);
+
+                case "Double":
+                    return Convert.ToDouble(line);
+
+                case "IntList":
+                    var ints = new List<int>();
+                    foreach (var item in line.Split('.'))
+                    {
+                        ints.Add(Convert.ToInt32(item.Trim()));
+                    }
+                    return ints;
+
+                case "StringList":
+                    var val = line.Replace("\"", "").Split(',').ToList();
+                    return val;
+
+                case "DoubleList":
+                    var doubles = new List<double>();
+
+                    foreach (var item in line.Split('.'))
+                    {
+                        doubles.Add(Convert.ToDouble(item.Trim()));
+                    }
+                    return doubles;
+            }
+            return "Unknow data notation";
+        }
+
+        private string GetType(string line)
+        {
             if (line.StartsWith('\"') && line.EndsWith('\"'))
-                return line;
+                return "String";
+
             else if (line.StartsWith('[') && line.EndsWith(']'))
             {
                 line = line.Replace("[", "").Replace("]", "");
                 if (line.Contains('\"'))
                 {
-                    var val = line.Replace("\"", "").Trim().Split(',');
-                    return val;
+                    return "StringArray";
                 }
                 else if (line.Contains('.'))
                 {
-                    var val = line.Trim().Split(',');
-                    double[] doubles = new double[val.Length];
-                    double = Convert.ToDouble(val);
+                    return "DoubleArray";
                 }
-            }
+                else
+                {
+                    return "IntArray";
+                }
 
-            return "";
+            }
+            else if (line.Contains('.'))
+            {
+                return "Double";
+            }
+            else
+                return "Int";
         }
 
     }
